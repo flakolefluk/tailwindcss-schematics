658e26ae9cfe021950cef8424ce30bd82777304a
Severin Bühler <Sebubu@users.noreply.github.com>
2020-02-10 10:41:19 +0800

    Update webpack-prod.config.js

--- src/ng-add-setup/files/webpack/webpack-prod.config.js
+++ src/ng-add-setup/files/webpack/webpack-prod.config.js
@@ -11,11 +11,16 @@ module.exports = {
                 require("tailwindcss")("./tailwind.config.js"),
                 require("@fullhuman/postcss-purgecss")({
                   content: [
+                    "./src/index.html",
                     "./src/**/*.component.html",
                     "./src/**/*.component.ts"
                   ],
-                  defaultExtractor: content =>
-                    content.match(/[A-Za-z0-9-_:/]+/g) || []
+                  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+                  whitelist: [
+                    ':host',
+                    '::ng-deep',
+                    ':host-context'
+                  ]
                 }),
                 require("autoprefixer")
               ]
